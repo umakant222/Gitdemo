1ca0c796d04d66a78271f6e142572fca6ed056c5
Umakant Ram Bhosale <urbhosale02@gmail.com>
2023-05-02 21:38:15 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sum{
+
+    public static void main(String args[]){
+        int a = 25;
+        int b = 70;
+        
+        System.out.println(a+b);
+    }
+
+}
