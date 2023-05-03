af573976748d1db02cedd7cece62894918581865
Umakant Ram Bhosale <urbhosale02@gmail.com>
2023-05-03 20:34:47 +0530

    added multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Multiply{
+
+    public static void main(String args[]){
+        int a = 20;
+        int b = 20;
+        
+        System.out.println("The Product is : "+(a*b));
+    }
+
+}
